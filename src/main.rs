b1682710e612d71da17905d37c46b9078de5f6dd
Matthew Ross <mer148@pitt.edu>
2026-05-10 21:46:27 +0200

    Implementeed asic sequence of play

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
+use colored::Colorize;
 use dialoguer::Select;
-use rand::prelude::*;
-use std::collections::VecDeque;
+use std::collections::HashSet;
 use std::fmt;
-
+use std::vec;
 // State
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum Player {
     Civitates,
     Dux,
     Saxons,
     Scotti,
+}
+
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Player::Civitates => write!(f, "{}", "Civitates".blue()),
+            Player::Dux => write!(f, "{}", "Dux".red()),
+            Player::Saxons => write!(f, "{}", "Saxons".black()),
+            Player::Scotti => write!(f, "{}", "Scotti".green()),
+        }
+    }
+}
+
+impl fmt::Debug for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Player::Civitates => write!(f, "{}", "Civitates".blue()),
+            Player::Dux => write!(f, "{}", "Dux".red()),
+            Player::Saxons => write!(f, "{}", "Saxons".black()),
+            Player::Scotti => write!(f, "{}", "Scotti".green()),
+        }
+    }
 }
 
 enum Imperium {
@@ -40,49 +63,19 @@ struct EdgeTrack {
     scotti_renown_threshold: u8,
 }
 
-struct SequenceOfPlay {
-    eligible_players: Vec<Player>,
-    ineligible_players: Vec<Player>,
-    passed_players: Vec<Player>,
-    current_state: SequenceOfPlayState,
-    action_chains: Vec<ActionChain>,
-    selected_action_chain: usize,
-}
-
-enum SequenceOfPlayState {
-    FirstAction,
-    SecondAction,
-    Cleanup,
-    // TODO: may be unnecessary
-    Epoch,
-}
-
-struct ActionChain {
-    first: Actions,
-    second: Actions,
-}
-
-enum Actions {
-    Command,
-    Feat,
-    Event,
-    LimitedCommand,
-    Pass,
-}
-
 // Board
 
-struct Space {
+struct Space<'a> {
     name: String,
     space_type: SpaceType,
     terrain: Option<Terrain>,
-    adj_spaces: Vec<&Space>,
-    adj_seas: Vec<&Sea>,
+    adj_spaces: Vec<&'a Space<'a>>,
+    adj_seas: Vec<&'a Sea<'a>>,
     pop: u8,
     max_pop: u8,
     top_prosp: u8,
     bottom_prosp: u8,
-    stronghold_sites: Vec<StrongholdSite>,
+    stronghold_sites: Vec<StrongholdSite<'a>>,
     control: Option<Player>,
 }
 
@@ -91,16 +84,16 @@ enum SpaceType {
     City,
 }
 
-struct Sea {
+struct Sea<'a> {
     name: String,
     patrol: bool,
-    adj: Vec<Space>,
+    adj: Vec<&'a Space<'a>>,
 }
 
-struct StrongholdSite {
+struct StrongholdSite<'a> {
     name: String,
     site_type: StrongholdSiteType,
-    stronghold: Option<&Stronghold>,
+    stronghold: Option<&'a Stronghold>,
 }
 
 enum StrongholdSiteType {
@@ -163,26 +156,6 @@ struct DuxCasualties {
 struct DuxOutOfPlay {
     cavalry: u8,
 }
-
-/*
-    Sequence of play
-
-    FIRST just get turn taking with Command only -> Command only -> next round
-        Passing
-        Eligible factions
-        First acted faction
-        Second acted faction
-        Ineligible factions
-        THE LOOP
-
-    SECOND Limited commands
-
-    THIRD Command + Feat
-
-    FOURTH events (Command XOR event)
-
-    FIFTH the actual different options for first player (and how second is different)
-*/
 
 // Components
 
@@ -247,35 +220,165 @@ enum UnitClass {
     Warband,
 }
 
-/*
-    Sequence of Play
+// Cards
+struct Event {
+    eligibility: Vec<Player>,
+    unshaded: Option<u8>,
+    shaded: Option<u8>,
+    historical_notes: String,
+}
 
-    1. Set up the game
-    2. Draw the first card
-    3. Reveal the second card
-        If next card is Epoch, swap it with current
-    4. Players with playable Pivotal Events may play them
-        Follow trump rules
-    5. We are searching for first eligible
-    6. For the factions on the event
-        If the faction is eligible
-            May pass
-                continue
-            If searching for first eligible
-                May select first eligible options
-                    Set first eligible action
-                    We are searching for second eligible
-            If searching for second eligible
-                May select option
-                    Set second eligible option
-                    We are done with round
-        We are done with round
-    7. All ineligible and passed factions become eligible
-    8. All acted factions become ineligible
-    9. Draw next card
-    10. Reveal next card
-        
-*/
+struct CardManager<'a> {
+    deck: &'a mut Vec<Event>,
+    current_event: &'a Event,
+    upcoming_event: &'a Event,
+    discard: &'a mut Vec<Event>,
+}
+
+// Sequence of Play
+
+struct SequenceOfPlay {
+    eligible_players: HashSet<Player>,
+    ineligible_players: HashSet<Player>,
+    passed_players: HashSet<Player>,
+    first_acted_player: Option<Player>,
+    second_acted_player: Option<Player>,
+    current_state: SequenceOfPlayState,
+    action_chains: Vec<ActionChain>,
+    selected_action_chain: usize,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum SequenceOfPlayState {
+    FirstAction,
+    SecondAction,
+    Cleanup,
+    // TODO: may be unnecessary
+    Epoch,
+}
+
+struct ActionChain {
+    first: Action,
+    second: Action,
+}
+
+#[derive(Debug)]
+enum Action {
+    Command,
+    Feat,
+    Event,
+    LimitedCommand,
+    Pass,
+}
+
+impl fmt::Display for SequenceOfPlay {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Current state: {:?}\nEligible factions: {:#?}\nIneligible factions: {:#?}",
+            self.current_state, self.eligible_players, self.ineligible_players
+        )
+    }
+}
+
+impl SequenceOfPlay {
+    fn run_sequence_of_play(&mut self, card_view: &CardManager) {
+        self.current_state = SequenceOfPlayState::FirstAction;
+        println!("{}", self);
+        // Query pivotal
+        let mut curr: usize = 0;
+        let mut second_actions: Vec<&str> = vec![];
+        while self.current_state == SequenceOfPlayState::FirstAction && curr < 4 {
+            let player: Player = card_view.current_event.eligibility[curr].clone();
+            curr += 1;
+            if self.eligible_players.contains(&player) {
+                self.eligible_players.remove(&player);
+                let options: Vec<&str> = vec!["Command", "Command + Feat", "Event", "Pass"];
+                let selection = Select::new()
+                    .with_prompt(format!("{} select one of the following actions!", player))
+                    .items(&options)
+                    .interact()
+                    .unwrap();
+                println!("{:?} chose: {}", player, options[selection]);
+                if options[selection] != "Pass" {
+                    self.current_state = SequenceOfPlayState::SecondAction;
+                    self.first_acted_player = Some(player);
+                    match selection {
+                        0 => second_actions.push("LimitedCommand"),
+                        1 => {
+                            second_actions.push("Limited Command");
+                            second_actions.push("Event");
+                        }
+                        2 => second_actions.push("Command + Feat"),
+                        _ => println!("Impossible!"),
+                    }
+                    second_actions.push("Pass");
+                } else {
+                    self.passed_players.insert(player);
+                    self.eligible_players.remove(&player);
+                }
+            }
+        }
+
+        while self.current_state == SequenceOfPlayState::SecondAction && curr < 4 {
+            let player: Player = card_view.current_event.eligibility[curr].clone();
+            curr += 1;
+            if self.eligible_players.contains(&player) {
+                self.eligible_players.remove(&player);
+                let selection = Select::new()
+                    .with_prompt(format!("{} select one of the following actions!", player))
+                    .items(&second_actions)
+                    .interact()
+                    .unwrap();
+                println!("{:?} chose: {}", player, second_actions[selection]);
+                if second_actions[selection] != "Pass" {
+                    self.current_state = SequenceOfPlayState::Cleanup;
+                    self.second_acted_player = Some(player);
+                } else {
+                    self.passed_players.insert(player);
+                    self.eligible_players.remove(&player);
+                }
+            }
+        }
+
+        println!("Cleaning up...");
+        println!(
+            "Shifting {:#?} from passed to eligible...",
+            self.passed_players
+        );
+        self.eligible_players.extend(self.passed_players.drain());
+
+        println!(
+            "Shifting {:#?} from ineligible to eligible...",
+            self.ineligible_players
+        );
+        self.eligible_players
+            .extend(self.ineligible_players.drain());
+
+        match self.first_acted_player {
+            Some(player) => {
+                self.first_acted_player = None;
+                self.ineligible_players.insert(player);
+                println!("Setting {} to ineligible...", player);
+            }
+            None => {}
+        }
+        match self.second_acted_player {
+            Some(player) => {
+                self.second_acted_player = None;
+                self.ineligible_players.insert(player);
+                println!("Setting {} to ineligible...", player);
+            }
+            None => {}
+        }
+        // Current consideration is first in card order
+        // While first acted is none and current consideration is not none
+        // Query action
+        // If pass, assign and continue
+        // Else assign
+        // While second acted is none and current
+    }
+}
 
 // Commands
 
@@ -437,4 +540,51 @@ fn main() {
        2. Begin sequence of play
        3. Loop
     */
+    println!("{:?} {} {} {}", Player::Civitates, Player::Dux, Player::Saxons, Player::Scotti);
+    let mut sop: SequenceOfPlay = SequenceOfPlay {
+        eligible_players: HashSet::from([
+            Player::Civitates,
+            Player::Dux,
+            Player::Saxons,
+            Player::Scotti,
+        ]),
+        ineligible_players: HashSet::from([]),
+        passed_players: HashSet::from([]),
+        first_acted_player: None,
+        second_acted_player: None,
+        current_state: SequenceOfPlayState::FirstAction,
+        action_chains: vec![],
+        selected_action_chain: 0,
+    };
+    let mut test_card_0: Event = Event {
+        eligibility: vec![
+            Player::Civitates,
+            Player::Dux,
+            Player::Saxons,
+            Player::Scotti,
+        ],
+        unshaded: None,
+        shaded: None,
+        historical_notes: String::from(""),
+    };
+    let mut test_card_1: Event = Event {
+        eligibility: vec![
+            Player::Scotti,
+            Player::Saxons,
+            Player::Dux,
+            Player::Civitates,
+        ],
+        unshaded: None,
+        shaded: None,
+        historical_notes: String::from(""),
+    };
+    let mut test_card_view: CardManager = CardManager {
+        deck: &mut vec![],
+        current_event: &test_card_0,
+        upcoming_event: &test_card_1,
+        discard: &mut vec![],
+    };
+    sop.run_sequence_of_play(&test_card_view);
+    test_card_view.current_event = test_card_view.upcoming_event;
+    sop.run_sequence_of_play(&test_card_view);
 }
